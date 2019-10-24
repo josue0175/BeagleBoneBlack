0bfe212f56ef0c529ccfe347948f172ce999ade9
josue0175 <josue0175@gmail.com>
2019-10-24 15:54:10 -0400

    first commit

---
+++ README.txt
@@ -0,0 +1,1 @@
+This is a repo for research/study on Embedded Hardware with the BeagleBone Black SoC
---
+++ format_sdcard.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -x
+
+if [ $(id -u) -ne 0 ]; then
+  echo "Please run this script as root"
+  exit
+fi
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 mmc_device"
+  exit
+fi
+
+#exec 1>&-
+#exec 2>&-
+
+dd if=/dev/zero of=$1 bs=1M count=16
+sfdisk $1 <<EOF
+1,99168,0xE,*
+,,,-
+EOF
+
+mkfs.vfat -F 16 ${1}p1 -n boot
+mkfs.ext4 ${1}p2 -L rootfs
+
+exit 0
